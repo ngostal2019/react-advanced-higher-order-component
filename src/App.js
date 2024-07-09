5d90ff3b75af06831d35e234159ce65e00a9c59f
Staline Ngoma <ngomastaline@gmail.com>
2024-07-08 23:00:11 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
+
+const withMousePosition = (WrappedComponent) => {
+  return (props) => {
+    const  [mousePosition, setMousePosition] = useState({
+      x: 0,
+      y: 0,
+    })
+
+    useEffect(() => {
+      const handleMousePositionChange = (e) => {
+        setMousePosition({
+          x: e.clientX,
+          y: e.clientY,
+        })
+      }
+
+      window.addEventListener('mousemove', handleMousePositionChange);
+
+      return () => {
+        window.removeEventListener('mousemove', handleMousePositionChange);
+      }
+    }, []);
+
+    return (
+      <WrappedComponent {...props} mousePosition={mousePosition} />
+    )
+  }
+}
+
+const PanelMouseLogger = ({ mousePosition }) => {
+  if (!mousePosition) {
+    return null;
+  };
+  return (
+    <div className='BasicTracker'>
+      <h1>Mouse position:</h1>
+      <div className='Row'>
+        <span>x: {mousePosition.x}</span> <hr />
+        <span>y: {mousePosition.y}</span>
+      </div>
+    </div>
+  );
+};
+
+const PointMouseLogger = ({ mousePosition }) => {
+  if (!mousePosition) {
+    return null;
+  };
+  return (
+    <p>
+      ({mousePosition.x}, {mousePosition.y})
+    </p>
+  );
+};
+
+const PanelMouseTracker = withMousePosition(PanelMouseLogger);
+const PointMouseTracker = withMousePosition(PointMouseLogger);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className='App-header'>Little Lemon Restaurant 🍋</header>
+      <PanelMouseTracker />
+      <PointMouseTracker />
     </div>
   );
 }
